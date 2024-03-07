bb29a59d199e27b4a51a822ea294ba23f1aad752
Muhammad Ahad <ahadsts990@gmail.com>
2024-03-07 16:22:09 +0500

    font

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,9 +1,6 @@
 import "./globals.css";
 import { Providers } from "./redux/StorePovider";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Nestjs Chat App",
@@ -17,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <Providers>
           {children}
         </Providers>
